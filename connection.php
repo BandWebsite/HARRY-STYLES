70dd40d29175e46d4aed2c5e57081fee236e93ba
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2022-07-11 21:53:56 -0700

    database fix

--- connection.php
+++ connection.php
@@ -1,8 +1,10 @@
 <?php
 	//development connection
-    //$con=mysqli_connect('localhost','root','','demo');
+    	//$con=mysqli_connect('localhost','root','','demo');
 	//remote connection
-	$con=mysqli_connect('remotemysql.com','PpSuXQTkqM','nyaPj3LQlP','PpSuXQTkqM');
+	//$con=mysqli_connect('remotemysql.com','PpSuXQTkqM','nyaPj3LQlP','PpSuXQTkqM');
+	//remote connection 2
+	$con = mysqli_connect('sql6.freesqldatabase.com', 'sql6504121', 'vAUQ4ICbZm', 'sql6504121')
 
     if(!$con)
     {
